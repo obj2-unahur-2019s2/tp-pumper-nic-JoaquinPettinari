1001ac0643e31467b91731c8b718b4aac8628b00
JoaquinPettinari <joaquinpettinari@hotmail.com>
2019-09-17 19:36:27 -0300

    Soy JOACO

--- src/main/java/ar/edu/unahur/obj2/HamburguesaCarne.java
+++ src/main/java/ar/edu/unahur/obj2/HamburguesaCarne.java
@@ -1,7 +1,7 @@
 package ar.edu.unahur.obj2;
 
 public class HamburguesaCarne extends Hamburguesa {
-
+    //Probando
     @Override
     public int getPrecio() {
         return super.getPrecio() + 200;
--- src/test/java/ar/edu/unahur/obj2/PapaFritaTest.java
+++ src/test/java/ar/edu/unahur/obj2/PapaFritaTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
+
 public class PapaFritaTest {
 
     PapaFrita g1;
